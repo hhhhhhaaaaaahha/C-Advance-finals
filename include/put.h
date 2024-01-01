41ef6c6bef9b33764058026692549ed4e28036aa
cardze <j10108jason@gmail.com>
2024-01-01 18:20:39 +0800

    please ignore the function

--- include/put.h
+++ include/put.h
@@ -20,7 +20,7 @@ typedef struct {
 int open_file_for_test(const char * path, Temp_Inode * inode);
 int open_file(const char * path, node * inode);
 node * put_file(file_system *fs, const char *path);
-node * put_folder(file_system *fs, const char *path);
+node * put_folder(file_system *fs, const char *path); // please ignore this function
 int read_file_buffer(Temp_Inode * inode, FILE * file, int size);
 
 int check_node_size(const char * path);
